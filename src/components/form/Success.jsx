0fe9eb078948227b33061b80d4528b7765884755
fbrv01 <fbucholc@gmail.com>
2022-07-11 10:37:37 +0200

    fixed missing exclamation in success message

--- src/components/form/Success.jsx
+++ src/components/form/Success.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Success = ({ formData }) => {
 	return (
 		<div className="form__success-message">
-			<h4>Good job {formData.firstname}</h4>
+			<h4>Good job {formData.firstname}!</h4>
 			<p>We have sent you an email to <span className="--bold">{formData.email}</span>.<br/>Make sure to click the link from the message to activate your account.</p>
 			<a href="./" className="button">Go to homepage</a>
 		</div>
